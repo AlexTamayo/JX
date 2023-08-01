7134688c6950a9016eb3dedb1066abbb3ae0ffdf
Alex Tamayo <catd00@gmail.com>
2023-08-01 02:53:14 -0700

    Add fake data, add scraped marketplace data

---
+++ db/schema/04_wishlists.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS wishlists CASCADE;
+CREATE TABLE wishlists (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  description TEXT
+);
+
+DROP TABLE IF EXISTS wishlist_contents CASCADE;
+CREATE TABLE wishlist_contents (
+  id SERIAL PRIMARY KEY NOT NULL,
+  wishlist_id INTEGER REFERENCES wishlists(id) ON DELETE CASCADE,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+  date_added TIMESTAMP NOT NULL,
+  sort_order INTEGER,
+  availability INTEGER
+);
