86f0b7362216cdee3e307234418751b54c108c88
Alex Tamayo <catd00@gmail.com>
2023-08-02 12:31:10 -0700

    Update web app template

--- db/connection.js
+++ db/connection.js
@@ -13,4 +13,268 @@ const db = new Pool(dbParams);
 
 db.connect();
 
-module.exports = db;
+
+
+/// Users
+
+const getUserWithEmail = function(email) {
+  const queryStr = `
+  SELECT *
+  FROM
+    users
+  WHERE
+    email = $1;    
+  `;
+
+  const values = [email];
+
+  return db
+    .query(queryStr, values)
+    .then(result => {
+      if (!result.rows[0]) return null;
+      return result.rows[0];
+    })
+    .catch(err => console.log(err.message));
+};
+
+const getUserWithId = function(id) {
+  const queryStr = `
+  SELECT *
+  FROM
+    users
+  WHERE
+    id = $1;    
+  `;
+
+  const values = [id];
+
+  return db
+    .query(queryStr, values)
+    .then(result => {
+      if (!result.rows[0]) return null;
+      return result.rows[0];
+    })
+    .catch(err => console.log(err.message));
+};
+
+const addUser = function(user) {
+
+  const queryStr = `
+  INSERT INTO
+    users (name, email, password)
+  VALUES
+    ($1, $2, $3)
+  RETURNING *;
+  `;
+
+  const values = [user.name, user.email, user.password];
+
+  return db
+    .query(queryStr, values)
+    .then(result => {
+      console.log(result.rows[0]);
+      result.rows[0]})
+    .catch(err => console.log(err.message));
+};
+
+/// Reservations
+
+const getAllReservations = function(guest_id, limit = 10) {
+
+  const queryStr = `
+  SELECT
+    properties.*,
+    avg(property_reviews.rating) as average_rating
+  FROM
+    reservations
+  JOIN
+    properties ON properties.id = reservations.property_id
+  JOIN
+    property_reviews ON properties.id = property_reviews.property_id
+  WHERE
+    reservations.guest_id = $1
+  GROUP BY
+    properties.id,
+    reservations.start_date
+  ORDER BY
+    reservations.start_date
+  LIMIT
+  $2;
+  `;
+
+  const values = [850, limit];
+
+  // const values = [guest_id, limit];
+
+  return db
+    .query(queryStr, values)
+    .then(result => result.rows)
+    .catch(err => console.log(err.message));
+
+};
+
+/// Properties
+
+
+// const getAllProperties = (options, limit = 10) => {
+
+//   const queryParams = [];
+
+//   let queryStr = `
+//   SELECT
+//     properties.*,
+//     avg(property_reviews.rating) as average_rating
+//   FROM
+//     properties
+//   JOIN
+//     property_reviews ON properties.id = property_id
+//   `;
+
+//   const whereClauses = [];
+
+//   //  owner_id | number_of_listings 
+//   // ----------+--------------------
+//   //     850   |         5
+//   //     818   |         5
+//   //     654   |         5
+//   //     617   |         5
+//   //     401   |         5
+
+//   if (options.owner_id) {
+//     // queryParams.push(`${options.owner_id}`);
+//     queryParams.push(850);
+//     whereClauses.push(`properties.owner_id = $${queryParams.length}`);
+//   }
+
+//   if (options.city) {
+//     queryParams.push(`%${options.city}%`);
+//     whereClauses.push(`city LIKE $${queryParams.length}`);
+//   }
+
+//   if (options.minimum_price_per_night) {
+//     queryParams.push(`${options.minimum_price_per_night * 100}`);
+//     whereClauses.push(`cost_per_night >= $${queryParams.length}`);
+//   }
+
+//   if (options.maximum_price_per_night) {
+//     queryParams.push(`${options.maximum_price_per_night * 100}`);
+//     whereClauses.push(`cost_per_night <= $${queryParams.length}`);
+//   }
+
+//   if (whereClauses.length > 0) {
+//     queryStr += ' WHERE ' + whereClauses.join(' AND ');
+//   }
+
+//   queryStr += `
+//   GROUP BY
+//     properties.id
+//   `;
+
+//   if (options.minimum_rating) {
+//     queryParams.push(`${options.minimum_rating}`);
+//     queryStr += `HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+
+//   }
+
+//   queryParams.push(`${limit}`);
+//   queryStr += `
+//   ORDER BY
+//     cost_per_night
+//   LIMIT
+//     $${queryParams.length};
+//   `;
+
+//   return db
+//     .query(queryStr, queryParams)
+//     .then(result => result.rows)
+//     .catch(err => console.log(err.message));
+// };
+
+const getAllProperties = (options, limit = 10) => {
+
+  const queryParams = [];
+
+  let queryStr = `
+  SELECT *
+  FROM
+    items
+  JOIN
+    item_images ON item_id = items.id
+  LIMIT
+    10;
+  `;
+
+  return db
+    .query(queryStr, queryParams)
+    .then(result => result.rows)
+    .catch(err => console.log(err.message));
+};
+
+
+const addProperty = function(property) {
+
+  const queryStr = `
+    INSERT INTO
+      properties (owner_id,
+                  title,
+                  description,
+                  thumbnail_photo_url,
+                  cover_photo_url,
+                  cost_per_night,
+                  parking_spaces,
+                  number_of_bathrooms,
+                  number_of_bedrooms,
+                  country,
+                  street,
+                  city,
+                  province,
+                  post_code,
+                  active) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, true) 
+    RETURNING *;
+  `;
+
+  const values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night * 100,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+  ];
+
+  return db
+    .query(queryStr, values)
+    .then(result => result.rows)
+    .catch(err => console.log(err.message));
+};
+
+// module.exports = db;
+
+// module.exports = {
+//   getUserWithEmail,
+//   getUserWithId,
+//   addUser,
+//   getAllReservations,
+//   getAllProperties,
+//   addProperty,
+// };
+
+module.exports = {
+  db,
+  getUserWithEmail,
+  getUserWithId,
+  addUser,
+  getAllReservations,
+  getAllProperties,
+  addProperty,
+};
+
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // load .env data into process.env
 require('dotenv').config();
 
+
 // Web server config
+// const path = require("path");
 const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
+const express = require("express");
 const morgan = require('morgan');
+const cookieSession = require("cookie-session");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }));
 app.use(
   '/styles',
   sassMiddleware({
@@ -25,27 +26,30 @@ app.use(
   })
 );
 app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, "./public")));
+// app.use(express.json());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
+  })
+);
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const usersRoutes = require('./routes/users');
+const apiRoutes = require("./routes/apis");
+const userRoutes = require("./routes/users");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/users', usersRoutes);
-// Note: mount other resources here, using the same pattern above
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
+// /api/endpoints
+app.use("/api", apiRoutes);
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// /user/endpoints
+app.use("/users", userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`JX app listening on port ${PORT}`);
+app.listen(PORT, (err) => {
+  console.log(err || `JX is listening on port ${PORT}`);
 });
