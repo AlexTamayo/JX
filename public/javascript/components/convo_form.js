c188ed9ec30ffa8875ae3e5895253e03541fd45d
Joshua Baker <joshuadfbaker@gmail.com>
2023-08-03 01:50:07 -0700

    displayMessage function added to conversation form

--- public/javascript/components/convo_form.js
+++ public/javascript/components/convo_form.js
@@ -1,7 +1,7 @@
 $(() => {
   const $convo_form = $(`
   <article class="chat-container">
-  
+
     <div class="chat__top">
       <div class="user">
         <img src="${escape(data.user.avatars)}" alt="avatar">
@@ -12,7 +12,7 @@ $(() => {
      </div>
    </div>
 
-    <div class="conversation">${escape(data.content.text)}</div>
+    <div class="convoContainer">${escape(data.content.text)}</div>
 
    <div class="chat__bottom">
       <div class="timestamp">${timeago.format(data.created_at)}</div>
@@ -21,6 +21,37 @@ $(() => {
 
   </article>
   `);
+
+
+  function displayMessages(messages) {
+    const convoContainer = $convo_form.find(".convoContainer");
+    convoContainer.empty();
+
+    messages.forEach((message) => {
+      const messageElement = $("").addClass("message");
+      messageElement.text(`${escape(message.user)}: ${escape(message.text)}`);
+      convoContainer.append(messageElement);
+    });
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const replyInput = $convo_form.find(".replyInput");
+    const reply = replyInput.val().trim();
+
+    if (reply !== "") {
+      const newMessage = { user: "User1", text: reply } // replace User1 with dynamic database
+    }
+
+  }
+
+
+
+
+
+
+
   window.$convo_form = $convo_form;
 
   $convo_form.on("submit", function (event) {
