55289a8fcd08aeaf3149b02ef237bac4fd0ad3c3
Alex Tamayo <catd00@gmail.com>
2023-08-04 01:17:06 -0700

    Add image to item description, remove some elements that need styling

--- public/javascript/components/item_description.js
+++ public/javascript/components/item_description.js
@@ -3,10 +3,11 @@ $(() => {
   
   function createDescription(data) {
     item = data.items[0];
-    // console.log(item);
     return `
     <article class="item-container">
-      <div class="item-img"></div>
+      <div class="item-img">
+        <img src="${item.image_1}" alt="${item.title} Image">
+      </div>
 
       <div class="item-info">
         <div class="title">${item.title}</div>
@@ -19,12 +20,6 @@ $(() => {
           <div class= "description_contetnt">
             <p>${item.description}</p>
           </div>
-        </div>
-        <div class="category">
-          <p><strong>Category</strong>&nbsp;&nbsp;&nbsp;&nbsp;${item.category}</p>
-        </div>
-        <div class="condition">
-          <p><strong>Condition</strong>&nbsp;&nbsp;&nbsp;&nbsp;${item.condition}</p>
         </div>
         <div class="location">
           <p><strong>Location</strong>&nbsp;&nbsp;&nbsp;&nbsp;${item.city}, ${item.province}</p>
